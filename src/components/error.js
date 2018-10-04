583d98ad2b7e5908e5e8cbddafb54b11667b433f
ayhankuru <ayhankuru@yandex.com.tr>
2018-10-05 00:16:17 +0300

    close #5

---
+++ src/components/error.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default () => (
+    <div className="error"> Bir sorun oluştu. </div>
+)
--- src/components/gate.js
+++ src/components/gate.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
+import Megaera from 'react-megaera'
 import Layout from './layout'
 import Auth from '../containers/auth'
+import Error from './error'
 
 const Gate = (props) => {
-          
-    return <React.Fragment>
-        {props.auth.isAuth ? <Layout {...props} /> : <Auth {...props} />}
-      </React.Fragment>
+  return (
+    <Megaera template={<Error />}>
+      {props.auth.isAuth ? <Layout {...props} /> : <Auth {...props} />}
+    </Megaera>
+  )
 }
 
 const mapStateToProps = ({ auth }) => ({
-    auth
+  auth
 })
-
 
 export default withRouter(connect(mapStateToProps)(Gate))
